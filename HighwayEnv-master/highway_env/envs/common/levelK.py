8941c2d7eb93865e857413162a8400bcbb8a9a72
QC <qincheng037@gmail.com>
2025-01-19 19:27:51 +0800

    保存修改，可能后续需要改进levelk的算法

--- HighwayEnv-master/highway_env/envs/common/levelK.py
+++ HighwayEnv-master/highway_env/envs/common/levelK.py
@@ -6,7 +6,8 @@ from copy import deepcopy
 
 class LevelK:
     def __init__(self):
-        self.actions = ["SLOWER", "IDLE", "FASTER"]
+        pass
+        self.actionslist = ["SLOWER", "IDLE", "FASTER"]
 
     def get_acceleration(self, env, vehicle_list, time_step=0.5):
         # 保存动作列表，按照受控车的顺序
@@ -20,9 +21,9 @@ class LevelK:
             index = -1
             minvalue = np.inf
             
-            for j in range(4-i):
-                if not selected[j] and minvalue > vehicle_list[j].levelK:
-                    minvalue = vehicle_list[j].levelK
+            for j in range(4):
+                if not selected[j] and minvalue > vehicle_list[j].levelk:
+                    minvalue = vehicle_list[j].levelk
                     index = j
                     
             vehicle = vehicle_list[index]
@@ -40,28 +41,27 @@ class LevelK:
             best_action,best_vehicle_reward = self.choose_best_action(env, vehicle)
             vehicle.act(best_action)
             env.road.step(1 / env.config["simulation_frequency"])
-            best_actions[i] = best_action
+            best_actions[index] = best_action
             best_reward += best_vehicle_reward
 
         return best_actions,best_reward
 
 
-    def choose_best_action(self, env, vehicle):
-        
-        copy_env = copy.deepcopy(env)
-        copy_vehicle = copy.deepcopy(vehicle)
-        
+    def choose_best_action(self, env, vehicle):      
         best_action = None
         best_reward = -np.inf
 
-        for action in self.actions:
+        for action in self.actionslist:
+            copy_env = copy.deepcopy(env)
+            copy_vehicle = copy.deepcopy(vehicle)
             # 更新该车辆（vehicle）的切向加速度和径向加速度
             copy_vehicle.act(action)
             # 依据环境中每辆车的加速度更新车辆的状态
             copy_env.road.step(1 / copy_env.config["simulation_frequency"])
 
             # 计算vehicle执行动作的奖励
-            predicted_reward = copy_env._agent_reward(action,copy_vehicle)
+            # predicted_reward = copy_env._agent_reward(action,copy_vehicle)
+            predicted_reward = copy_env._reward(action)
 
             if predicted_reward > best_reward:
                 best_reward = predicted_reward
