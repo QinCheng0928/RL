0001466bef5ef2f53a318f52f1c07352184978ae
QC <qincheng037@gmail.com>
2025-02-09 17:45:37 +0800

    暂时保存训练模型

--- HighwayEnv-master/highway_env/envs/common/levelK2.py
+++ HighwayEnv-master/highway_env/envs/common/levelK2.py
@@ -29,6 +29,7 @@ class LevelK:
                                 i.speed=0
                             else:
                                 temp_env.controlled_vehicles[vehicle].act(cur_action)
+                        temp_env.road.act()
                         temp_env.road.step(1 / temp_env.config["simulation_frequency"])
                         predicted_reward = temp_env._reward(cur_action)
                         if predicted_reward > maxvalue:
@@ -40,6 +41,7 @@ class LevelK:
                                 temp_env.controlled_vehicles[i].act(dp[i][k-1])
                             else:
                                 temp_env.controlled_vehicles[vehicle].act(cur_action)
+                        temp_env.road.act()
                         temp_env.road.step(1 / temp_env.config["simulation_frequency"])
                         predicted_reward = temp_env._reward(cur_action)
                         if predicted_reward > maxvalue:
